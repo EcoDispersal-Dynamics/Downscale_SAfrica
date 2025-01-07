353e65a0643a4672d8671197fc1134845e905f84
Markus Shiweda <Markus.Shiweda@kit.edu>
2025-01-07 08:17:08 +0100

    Installed LandScaleR-dev version

--- scripts/install_packages.R
+++ scripts/install_packages.R
@@ -15,16 +15,28 @@ install_if_needed('FNN')
 install_if_needed('terra') # Press no when RStudio asks if package should be compiled from source
 install_if_needed('data.table')
 install_if_needed('rgee')
-# Install and load LandScaleR from GitHub
-if (!requireNamespace("LandScaleR", quietly = TRUE)) {
-  devtools::install_github("TamsinWoodman/LandScaleR", build_vignettes = TRUE)
-} else {
-  message("LandScaleR is already installed.")
-}
+install_if_needed('Rcpp')
+# # Install and load LandScaleR from GitHub
+# if (!requireNamespace("LandScaleR", quietly = TRUE)) {
+#   devtools::install_github("TamsinWoodman/LandScaleR", build_vignettes = TRUE)
+# } else {
+#   message("LandScaleR is already installed.")
+# }
+# 
+# # Now load the package
+# if (requireNamespace("LandScaleR", quietly = TRUE)) {
+#   library(LandScaleR)
+# } else {
+#   stop("Failed to install or load the LandScaleR package.")
+# }
 
-# Now load the package
-if (requireNamespace("LandScaleR", quietly = TRUE)) {
-  library(LandScaleR)
-} else {
-  stop("Failed to install or load the LandScaleR package.")
-}
+
+# Install -dev version of LandScaleR from GitHub
+# 
+
+devtools::install_github("TamsinWoodman/LandScaleR-dev", ref = "dev", build_vignettes = TRUE)
+library(LandScaleR)
+# Confirm if dev version is the one installed
+packageDescription("LandScaleR")
+devtools::session_info()
+
